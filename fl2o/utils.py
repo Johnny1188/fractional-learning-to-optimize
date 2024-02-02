c65197084df34d810f60678fbd06a9a8f8f79763
Jan Sobotka <jsobotka1188@gmail.com>
2024-02-02 18:44:17 +0100

    add plot_metric util func

--- fl2o/utils.py
+++ fl2o/utils.py
@@ -1,6 +1,7 @@
 import functools
 import numpy as np
 from matplotlib import pyplot as plt
+import seaborn as sns
 from copy import deepcopy
 
 
@@ -119,3 +120,204 @@ def plotter(to_plot, run_log, meta_training_log, run_num, **kwargs):
     plt.plot(run_log[to_plot]),
     plt.title(f"{to_plot} (run {run_num})"),
     plt.show()
+
+
+def plot_metric(
+    baselines,
+    l2os,
+    metric,
+    show_max_iters=None,
+    log_metric=False,
+    with_err_bars=False,
+    conv_window=None,
+    save_fig_to_path=None,
+):
+    ### plot comparison
+    fig = plt.figure(figsize=(9, 6))
+    ax = fig.add_subplot(111)
+
+    ### baseline optimizers
+    for baseline_name, baseline_dict in baselines.items():
+        opter_metric = np.array(baseline_dict["log"][metric])
+        plot_config = baseline_dict["plot_config"] if "plot_config" in baseline_dict else None
+        
+        if show_max_iters is None:
+            x = range(opter_metric.shape[1])
+            y = np.mean(opter_metric, axis=0)
+        else:
+            x = range(opter_metric[..., :show_max_iters].shape[1])
+            y = np.mean(opter_metric[..., :show_max_iters], axis=0)
+        if conv_window and conv_window > 1:
+            y_removed_start = y[:conv_window - 1]
+            y = np.convolve(y, np.ones(conv_window), "valid") / conv_window
+            y = np.concatenate([y_removed_start, y])
+        
+        if plot_config:
+            sns.lineplot(
+                x=x,
+                y=y,
+                label=baseline_name,
+                ax=ax,
+                **plot_config["main"],
+            )
+        else:
+            sns.lineplot(
+                x=x,
+                y=y,
+                label=baseline_name,
+                ax=ax,
+            )
+
+        if with_err_bars:
+            if show_max_iters is None:
+                y_std = np.std(opter_metric, axis=0)
+            else:
+                y_std = np.std(opter_metric[..., :show_max_iters], axis=0)
+            if conv_window and conv_window > 1:
+                y_removed_start = y_std[:conv_window - 1]
+                y_std = np.convolve(y_std, np.ones(conv_window), "valid") / conv_window
+                y_std = np.concatenate([y_removed_start, y_std])
+            if plot_config and plot_config["err_bars"]:
+                sns.lineplot(
+                    x=x,
+                    y=y_std,
+                    ax=ax,
+                    **plot_config["err_bars"],
+                )
+            else:
+                ax.fill_between(
+                    x,
+                    y - y_std,
+                    y + y_std,
+                    alpha=0.2,
+                )
+            
+
+    ### L2O optimizers
+    for l2o_name, l2o_dict in l2os.items():
+        l2o_metric = np.array(l2o_dict["log"][metric])
+        plot_config = l2o_dict["plot_config"] if "plot_config" in l2o_dict else None
+
+        if show_max_iters is None:
+            x = range(l2o_metric.shape[1])
+            y = np.mean(l2o_metric, axis=0)
+        else:
+            x = range(l2o_metric[:,:show_max_iters].shape[1])
+            y = np.mean(l2o_metric[:,:show_max_iters], axis=0)
+        if conv_window and conv_window > 1:
+            y_removed_start = y[:conv_window - 1]
+            y = np.convolve(y, np.ones(conv_window), "valid") / conv_window
+            y = np.concatenate([y_removed_start, y])
+        if plot_config:
+            sns.lineplot(
+                x=x,
+                y=y,
+                label=fr"{l2o_name}",
+                ax=ax,
+                **plot_config["main"],
+            )
+        else:
+            sns.lineplot(
+                x=x,
+                y=y,
+                label=fr"{l2o_name}",
+                ax=ax,
+            )
+
+        if with_err_bars:
+            if show_max_iters is None:
+                y_std = np.std(l2o_metric, axis=0)
+            else:
+                y_std = np.std(l2o_metric[:,:show_max_iters], axis=0)
+            if conv_window and conv_window > 1:
+                y_removed_start = y_std[:conv_window - 1]
+                y_std = np.convolve(y_std, np.ones(conv_window), "valid") / conv_window
+                y_std = np.concatenate([y_removed_start, y_std])
+            if plot_config and plot_config["err_bars"]:
+                sns.lineplot(
+                    x=x,
+                    y=y_std,
+                    ax=ax,
+                    **plot_config["err_bars"],
+                )
+            else:
+                ax.fill_between(
+                    x,
+                    y - y_std,
+                    y + y_std,
+                    alpha=0.2,
+                )
+
+    ### plot settings
+    # y-label
+    ax.set_xlabel("Iteration")
+    if metric == "loss":
+        metric_as_label = "Loss"
+    else:
+        metric_as_label = metric
+    if log_metric:
+        metric_as_label = f"{metric_as_label} (log scale)"
+    ax.set_ylabel(metric_as_label)
+
+    # scale of y-axis
+    if log_metric:
+        ax.set_yscale("log")
+    else:
+        ax.set_ylim(0.0, None)
+
+    # legend
+    ax.legend(loc="upper center", bbox_to_anchor=(0.5, 1.25), ncol=2)
+    legend = ax.get_legend()
+    for legend_handle in legend.legendHandles:
+        legend_handle.set_linewidth(3.0)
+
+    # x-ticks
+    x_ticks = ax.get_xticks()
+    x_ticks = np.linspace(0, len(x), 3)
+    ax.set_xticks(x_ticks)
+
+    ### y-ticks
+    if log_metric:
+        y_ticks = ax.get_yticks()
+        # y_ticks = np.linspace(y_ticks[1], y_ticks[-2], 2)
+        y_ticks = [y_ticks[idx] for idx in range(1, len(y_ticks) - 1, len(y_ticks) // 3)]
+        ax.set_yticks(y_ticks)
+
+        # ax.set_ylim(0.185, 2.8)
+        # ax.set_yticks([0.3, 1, 2])
+        # ax.set_yticklabels([0.3, 1, 2])
+
+        # ax.set_yticks([0.2, 1, 2])
+        # ax.set_yticklabels([0.2, 1, 2])
+    else:
+        y_max = ax.get_ylim()[1]
+        y_max = np.ceil(y_max / 0.5) * 0.5 # round
+        y_ticks = np.linspace(0, y_max, 3)
+        ax.set_yticks(y_ticks)
+
+    # axins.set_xlim(0, 80)
+    # axins.set_ylim(0.35, 2.5)
+    # # axins.set_yscale("log")
+    # axins.set_xticks([0, 40, 80])
+    # # axins.set_yticks([1.])
+    # axins.set_yticklabels([1.0], fontsize=7)
+    # axins.set_xticklabels([0, 40, 80], fontsize=7.5, position=(0., .08))
+    # ax.indicate_inset_zoom(axins)
+        
+    ### remove top and right axis
+    ax.spines["right"].set_visible(False)
+    ax.spines["top"].set_visible(False)
+
+    ### remove legend's frame
+    legend = ax.get_legend()
+    legend.set_frame_on(False)
+
+    # Add tick lines for both major and minor ticks
+    ax.tick_params(which="major", length=3, bottom=True, left=True)
+    ax.tick_params(which="minor", length=3, bottom=False, left=False)
+
+    plt.show()
+
+    ### save the figure
+    if save_fig_to_path is not None:
+        fig.savefig(save_fig_to_path, bbox_inches="tight")
